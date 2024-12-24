492d06bd0b00037174563b2dcffd743592c971bd
Estela Sato <estela.sato@hotmail.com>
2024-12-24 00:23:57 -0300

    add location components

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -4,48 +4,119 @@ import { Places } from "@/components/places";
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
 import { Alert, Text, View } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import { colors } from "@/styles/colors";
+import { fontFamily } from "@/styles/font-family";
+import { router } from "expo-router";
 
+type MarketProps = PlaceProps & {
+  latitude: number;
+  longitude: number;
+}
+
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+};
 
 export default function Home() {
-  const [categories, setCategories] = useState<CategoriesProps>([])
-  const [category, setCategory] = useState<string>("")
+  const [categories, setCategories] = useState<CategoriesProps>([]);
+  const [category, setCategory] = useState<string>("");
 
-  const [markets, setMarkets] = useState<PlaceProps[]>([])
+  const [markets, setMarkets] = useState<MarketProps[]>([]);
 
   async function fetchCategories() {
     try {
-      const {data} = await api.get("/categories")
-      setCategories(data)
-      setCategory(data[0].id)
-    } catch(e) {
-      console.log(e)
-      Alert.alert("Categorias", "Não foi possível carregar as categorias.")
+      const { data } = await api.get("/categories");
+      setCategories(data);
+      setCategory(data[0].id);
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Categorias", "Não foi possível carregar as categorias.");
     }
   }
 
   async function fetchMarket() {
     try {
-      if (!category) return
-      const {data} = await api.get(`/markets/category/${category}`)
-      setMarkets(data)
-    } catch(e) {
-      console.log(e)
-      Alert.alert("Locais", "Não foi possível carregar os locais.")
+      if (!category) return;
+      const { data } = await api.get(`/markets/category/${category}`);
+      setMarkets(data);
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Locais", "Não foi possível carregar os locais.");
+    }
+  }
+
+  async function getCurrentLocation() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync({});
+        console.log(location);
+      }
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Localização", "Não foi possível obter a localização.");
     }
   }
 
   useEffect(() => {
-    fetchCategories()
-  }, [])
+    // getCurrentLocation()
+    fetchCategories();
+  }, []);
 
   useEffect(() => {
-    fetchMarket()
-  }, [category])
+    fetchMarket();
+  }, [category]);
 
   return (
-    <View style={{ flex: 1}}>
-      <Categories data={categories} selected={category} onSelected={setCategory}/>
-      <Places data={markets}/>
+    <View style={{ flex: 1 }}>
+      <Categories
+        data={categories}
+        selected={category}
+        onSelected={setCategory}
+      />
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        {/* exibe localização atual */}
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
+
+        {markets?.map((item: MarketProps) => (
+          <Marker key={item.id} identifier={item.id}
+          coordinate={{
+            latitude: item.latitude,
+            longitude: item.longitude,
+          }}
+          image={require("@/assets/pin.png")}
+          >
+            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+              <View>
+                <Text style={{ fontSize: 14, color: colors.gray[600], fontFamily: fontFamily.medium}}>{item.name}</Text>
+                <Text  style={{ fontSize: 12, color: colors.gray[600], fontFamily: fontFamily.regular}}>{item.address}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+
+      <Places data={markets} />
     </View>
-  )
-}
+  );
+} 
